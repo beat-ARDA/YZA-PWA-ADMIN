905be6f15eb07bc7b927d97cde1187084c99aa6c
AlvaroDuron <e-arduron@neoris.com>
2022-01-25 18:58:06 -0600

    Ruta agregar periodo

--- YZA-PWA-ADMIN/src/app/app-routing.module.ts
+++ YZA-PWA-ADMIN/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PantallaPrincipalComponent } from './pantalla-principal/pantalla-principal.component';
 
-const routes: Routes = [{ path: '', component: PantallaPrincipalComponent }];
+const routes: Routes = [{ path: 'agregar-periodo', component: PantallaPrincipalComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
