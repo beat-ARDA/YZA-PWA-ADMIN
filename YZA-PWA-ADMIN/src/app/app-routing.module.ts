74e2503e800ff4271826b53dc75bf42958bc8435
AlvaroDuron <e-arduron@neoris.com>
2022-01-25 18:53:35 -0600

    Pantalla principal

--- YZA-PWA-ADMIN/src/app/app-routing.module.ts
+++ YZA-PWA-ADMIN/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PantallaPrincipalComponent } from './pantalla-principal/pantalla-principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: PantallaPrincipalComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- YZA-PWA-ADMIN/src/app/app.module.ts
+++ YZA-PWA-ADMIN/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PantallaPrincipalComponent } from './pantalla-principal/pantalla-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PantallaPrincipalComponent
   ],
   imports: [
     BrowserModule,
