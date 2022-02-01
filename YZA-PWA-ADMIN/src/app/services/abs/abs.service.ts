7396413e63aa7fd7430ff4a606814b92a58b8d0c
AlvaroDuron <e-arduron@neoris.com>
2022-01-31 22:45:54 -0600

    abs service

--- YZA-PWA-ADMIN/src/app/services/abs/abs.service.ts
+++ YZA-PWA-ADMIN/src/app/services/abs/abs.service.ts
@@ -7,14 +7,13 @@ import { APService } from '../ap/ap.service';
 })
 export class ABSService {
 
-  public blobSasUrl = "https://yza.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2022-01-28T01:22:26Z&st=2022-01-27T17:22:26Z&spr=https&sig=MdHoNEUuQxacHI6%2FP9B3uLe3ncCO9e6t1W6pq2LDu7M%3D";
+  public blobSasUrl = "https://yza.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2022-01-29T04:56:28Z&st=2022-01-28T20:56:28Z&spr=https&sig=E0F4bcazA%2BfZfwxYiPpK93nlFGDR3kEGOYVJsh1%2FcR4%3D";
   public blobServiceClient = new BlobServiceClient(this.blobSasUrl);
   public containerName = "yza-container";
   public containerClient = this.blobServiceClient.getContainerClient(this.containerName);
 
   constructor(private ps: APService) {
   }
-
   uploadFiles = async (inputFile: any, guid: string) => {
     try {
       const blockBlobClient = this.containerClient.getBlockBlobClient(inputFile.files[0].name);
