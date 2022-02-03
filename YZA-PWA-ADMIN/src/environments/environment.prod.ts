d3f9baf8a81f2c7971c02bfbbae07a49013fbb7f
Ricardo Cardeña Sanchez <ricardo.cardena@neoris.com>
2022-02-03 10:15:35 -0600

    adding prod enviroment

--- YZA-PWA-ADMIN/src/environments/environment.prod.ts
+++ YZA-PWA-ADMIN/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  STORAGE_URL: 'http://127.0.0.1:10000/devstoreaccount1/reportecanibalizacion',
+  API_URL: 'http://20.96.44.96:9090', 
+  ENDPOINT_PERIODO: '/api/Periodo',
+  ENDPOINT_EXCEL_FORMAT: '/api/File/ReporteCanibalizacion',
+  BLOB_SAS_URL: "https://yzapwablob.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2060-02-03T03:54:24Z&st=2022-02-02T19:54:24Z&spr=https&sig=UovLO0%2Bfpnmai2OoRWmPJar8sxokbYkEfjnEqeJ%2FbNw%3D",
+  BLOB_CONTAINER_NAME: "yza-container"
 };
