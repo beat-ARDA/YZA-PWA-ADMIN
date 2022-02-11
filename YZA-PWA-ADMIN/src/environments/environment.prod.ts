ceec8ee4ace50ffaf7c4d20d336b192f4b1ad25a
Ricardo Cardeña Sanchez <ricardo.cardena@neoris.com>
2022-02-11 11:16:18 -0600

    change blob name on prod enviroments

--- YZA-PWA-ADMIN/src/environments/environment.prod.ts
+++ YZA-PWA-ADMIN/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   ENDPOINT_PERIODO: '/api/Periodo',
   ENDPOINT_EXCEL_FORMAT: '/api/File/ReporteCanibalizacion',
   BLOB_SAS_URL: "https://yzapwablob.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2060-02-03T03:54:24Z&st=2022-02-02T19:54:24Z&spr=https&sig=UovLO0%2Bfpnmai2OoRWmPJar8sxokbYkEfjnEqeJ%2FbNw%3D",
-  BLOB_CONTAINER_NAME: "yza-container",
+  BLOB_CONTAINER_NAME: "reportecanibalizacion",
   clienteId: '106a9363-5976-4a32-8967-50cfa2f769db',
   dirId: 'https://login.microsoftonline.com/66f9ab85-de1a-40d4-b7bb-21565ad57cce',
   redUrl: 'http://localhost:4200',
