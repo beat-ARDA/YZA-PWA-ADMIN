ad36b381eec6bed2622f8adcba46d56112ba0670
Ricardo Cardeña Sanchez <ricardo.cardena@neoris.com>
2022-02-11 11:36:37 -0600

    addinmg prod blob sas url

--- YZA-PWA-ADMIN/src/environments/environment.prod.ts
+++ YZA-PWA-ADMIN/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   API_URL: "https://apim-canibalizacion-poc-eastus2-01.azure-api.net",
   ENDPOINT_PERIODO: '/api/Periodo',
   ENDPOINT_EXCEL_FORMAT: '/api/File/ReporteCanibalizacion',
-  BLOB_SAS_URL: "https://yzapwablob.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2060-02-03T03:54:24Z&st=2022-02-02T19:54:24Z&spr=https&sig=UovLO0%2Bfpnmai2OoRWmPJar8sxokbYkEfjnEqeJ%2FbNw%3D",
+  BLOB_SAS_URL: "https://stcanibalizacionpoc02.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=co&sp=rwdlacupitfx&se=2050-02-12T01:28:08Z&st=2022-02-11T17:28:08Z&spr=https&sig=UaRQy9enzTqmVhMxUm1LsMXUuvk75rGdXprvTRKcNg4%3D",
   BLOB_CONTAINER_NAME: "reportecanibalizacion",
   clienteId: '106a9363-5976-4a32-8967-50cfa2f769db',
   dirId: 'https://login.microsoftonline.com/66f9ab85-de1a-40d4-b7bb-21565ad57cce',
